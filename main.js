43345795a82c6d13bab8a0c0067ce213f44c62bc
Quant85 <antonio.85.q@gmail.com>
2020-11-26 15:49:25 +0100

    Aggiunto descrizione metodi utilizzati - END

--- main.js
+++ main.js
@@ -6,15 +6,20 @@
  * https://bitbucket.org/booleancareers/ex-hamburger-menu-base/downloads
  *Ricordate di scrivere solo il codice JavaScript con jQuery e non modificare HTML e CSS
 *****************************************************************************************************************/ 
+//1. dichiaro le variabili per associare gli elementi che seleziono, in modo da renderli rimanipolabili all'occorrenza 
+var hamburgerIconSelect, hamburgerMenuSelect, hamburgerMenuSelectClose;
 
-var hamburgerIconSelect = $("div.header-right > a");
-var hamburgerMenuSelect = $(".hamburger-menu");
-var hamburgerMenuSelectClose = $(".hamburger-menu >a.close");
+hamburgerIconSelect = $("div.header-right > a");
+hamburgerMenuSelect = $(".hamburger-menu");
+hamburgerMenuSelectClose = $(".hamburger-menu >a.close");
 
+//2. uso i metodi propri di jQ come .click per poter azionare la funzione dicharata al suo interno nel momento in cui si effettua il click sulla selezione dichiarata nella variabile ad esso associato
+
+//2.1 Quando si effettua il click su "div.header-right > a" si attiva la funzione che permette di rendere "display: block" - (quanto meno visibile nel flusso), mediante il metodo .show(), l'elemento selezionato.
 hamburgerIconSelect.click(function () { 
   hamburgerMenuSelect.show();
 });
-
+//2.1 Quando si effettua il click su ".hamburger-menu >a.close" si attiva la funzione che permette di rendere "display: none" - (quanto meno non visibile nel flusso del document), mediante il metodo .hide(), l'elemento selezionato.
 hamburgerMenuSelectClose.click(function () { 
   hamburgerMenuSelect.hide();
 });
